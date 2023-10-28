39d098049a05d0c55cbf36a9e1d0e17bfa744586
janielala <janielaudesign@gmail.com>
2023-10-28 15:46:13 +0800

    My first app

---
+++ app.js
@@ -0,0 +1,21 @@
+let mybutton = document.querySelector("#click");
+mybutton.addEventListener("click", myClick);
+
+function myClick() {
+    let name = prompt("Hello! What is your name?")
+    if (name === ""|| null) {
+        name = "there";
+    }
+    let ans = prompt(`Hello ${name}. What is your favourite weather?`);
+    let myImage = document.querySelector("#image");
+    
+    if (ans.toLowerCase() === "sunny") {
+        myImage.src = "./asset/sunny.gif";
+    }
+    else if (ans.toLowerCase() === "rainy") {
+        myImage.src = "./asset/rainy.gif";
+    }
+    else {
+        alert("This weather is not in our library");
+    }
+}
